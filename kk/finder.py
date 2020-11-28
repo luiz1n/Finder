daaa184aaf92b80b43d2421be6e4d39f5e6646ed
Luiz1n <64563249+luiz1n@users.noreply.github.com>
2020-11-27 21:02:57 -0300

    Update finder.py

--- kk/finder.py
+++ kk/finder.py
@@ -1,23 +1,26 @@
+import os
+
+def Check():
+	try:
+		import requests
+	except ImportError:
+		os.system("python -m pip install requests")
+	except Exception:
+		pass
+Check()
 import requests
-try:
-	url = input('URL: ')
-	if 'http://' in url:
-		url = url.split('http://')
-		url = url[1].split('/')
-	elif "https://" in url:
-		url = url.split('https://')
-		url = url[1].split('/')
-	url = url[0]
-	arch = open('wordlist.txt', 'r')
-	for w in arch:
-		w = w.strip()
-		d = 'http://'+url+'/'+w
-		r = requests.get(d)
-		status = r.status_code
-		if status == 200:
-			print(f'{d} => Status: {status} (Sucesso!)')
-		else:
-			print(f'{d} => Status: {status} [ Falha :( ]')
-except KeyboardInterrupt:
-	print('Saindo...')
-	exit()
+
+def Finder():
+	try:
+		url = input('URL: ')
+		for word in open('wordlist.txt', 'r'):
+			r = requests.get(f'{url}/{word}')
+			if '404' in r.text:
+				print(f'{url}/{word} -> Falha!')
+			else:
+				print(f'{url}/{word} -> Sucesso!')
+	except KeyboardInterrupt:
+		exit()
+		os.close()
+
+Finder()
